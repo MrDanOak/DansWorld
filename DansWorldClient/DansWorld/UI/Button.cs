5cb95e5aff9ce691e6c7deca9caacdd344143d5e
Dan Scott <tehdanscott@gmail.com>
2018-03-08 10:33:47 +0000

    Initial Commit for new project

---
+++ DansWorldClient/DansWorld/UI/Button.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DansWorld.UI.CustomEventArgs;
+using DansWorld.Math;
+
+namespace DansWorld.UI
+{
+    class Button : Control
+    {
+        public string Text = "";
+        public Texture2D BackgroundImage;
+        public SpriteFont Font;
+        public Button()
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            if (BackgroundImage != null)
+            {
+
+            }
+            else if (BackColor != null)
+            {
+                spriteBatch.Draw(DansWorld.DEFAULT_TEXTURE, Destination, BackColor);
+                spriteBatch.DrawString(Font, Text, new Vector2(Location.X + Size.X / 2 - Font.MeasureString(Text).X / 2, 
+                    Location.Y + Size.Y / 2 - Font.MeasureString(Text).Y / 2), FrontColor);
+            }
+        }
+    }
+}
---
+++ DansWorldClient/DansWorld/UI/Image.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DansWorld.UI.CustomEventArgs;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DansWorld.UI
+{
+    class Image : Control
+    {
+        private Texture2D _texture;
+        private Rectangle? _source;
+        private float _rotation;
+        private Vector2 _origin;
+        private SpriteEffects _effects;
+        public Image(Texture2D texture)
+        {
+            _texture = texture;
+        }
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, Destination, _source, BackColor, _rotation, _origin, _effects, 0.0f);
+        }
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
